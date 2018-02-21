cf1672457370fda106f0e37c0588d335ed35ecdc
Aaron Arney <ocularrhythm@users.noreply.github.com>
2018-02-21 14:40:59 -0500

    Apply minor code formatting

--- lib/site.php
+++ lib/site.php
@@ -13,7 +13,7 @@ class TimberFoundationSite extends TimberRegionsSite {
 
     public $foundation_js_vars = array();
 
-    public function __construct(){
+    public function __construct() {
         $this->editor_style = 'assets/css/editor-style.css';
 
         $this->theme_supports[] = 'foundation-icons';
@@ -69,7 +69,7 @@ class TimberFoundationSite extends TimberRegionsSite {
         global $wp_styles;
 
         // @todo Add a filter for child themes to decide whether to load the parent styles or not
-        $uri = get_stylesheet_directory_uri() . '/assets/css/';
+        $uri = $this->_get_uri() . '/assets/css/';
 
         // foundation stylesheet
         wp_register_style( 'timber-foundation', $this->_get_uri() . '/assets/css/app.css', array(), '' );
@@ -99,9 +99,10 @@ class TimberFoundationSite extends TimberRegionsSite {
 
         //wp_enqueue_style( 'google-font' );
 
-        if ( current_theme_supports( 'foundation-icons' ) )
+        if ( current_theme_supports( 'foundation-icons' ) ) {
             wp_enqueue_style( 'foundation-icons' );
-
+        }
+        
         parent::enqueue_styles();
     }
 
@@ -113,7 +114,9 @@ class TimberFoundationSite extends TimberRegionsSite {
         wp_register_script( 'timber-foundation-modernizr', $uri . 'modernizr.js', array(), '2.6.2', false );
 
         // comment reply script for threaded comments
-        if( get_option( 'thread_comments' ) )  { wp_enqueue_script( 'comment-reply' ); }
+        if ( get_option( 'thread_comments' ) ) { 
+            wp_enqueue_script( 'comment-reply' );
+        }
 
         // adding Foundation scripts file in the footer
         $min = defined( 'SCRIPT_DEBUG' && SCRIPT_DEBUG ) ? '.min' : '';
@@ -151,11 +154,12 @@ class TimberFoundationSite extends TimberRegionsSite {
         return new TimberFoundationRegions;
     }
 
-    public function add_to_context( $context ){
+    public function add_to_context( $context ) {
         // Theme Options/Settings - prevent to run twice if child-themed
-        if ( !isset( $context['theme_options'] ) )
+        if ( !isset( $context['theme_options'] ) ) {
             $context['theme_options'] = new TimberFoundation_ThemeOptions();
-
+        }
+        
         $context = parent::add_to_context( $context );
 
         // Override the title and force the seperator on the right
@@ -169,8 +173,8 @@ class TimberFoundationSite extends TimberRegionsSite {
         $context['is_user_logged_in'] = is_user_logged_in();
         $context['current_user'] = ( is_user_logged_in() ) ? new TimberUser() : false;
 
-        $context['wp_login_url'] = TimberHelper::function_wrapper('wp_login_url', array( get_permalink() ) );
-        $context['wp_logout_url'] = TimberHelper::function_wrapper('wp_logout_url', array( get_permalink() ) );
+        $context['wp_login_url'] = TimberHelper::function_wrapper( 'wp_login_url', array( get_permalink() ) );
+        $context['wp_logout_url'] = TimberHelper::function_wrapper( 'wp_logout_url', array( get_permalink() ) );
 
         global $comment_author, $comment_author_email, $comment_author_url;
         $context['comment_author'] = $comment_author;
@@ -180,49 +184,49 @@ class TimberFoundationSite extends TimberRegionsSite {
         return $context;
     }
 
-        protected function add_menus_to_context( $context ) {
-            $context['menu'] = new TimberMenu( 'primary' );
-            $context['menu_left'] = TimberHelper::function_wrapper( 'wp_nav_menu', array( array(
-                'theme_location' => 'primary',
-                'container' => false,
-                'depth' => 0,
-                'items_wrap' => '<ul class="left">%3$s</ul>',
-                //'fallback_cb' => array( 'TimberFoundation_Walker', 'render_menu_fallback' ),
-                'fallback_cb' => false,
-                'walker' => new TimberFoundation_Walker( array(
-                    'in_top_bar' => true,
-                    'add_dividers' => true,
-                    'item_type' => 'li',
-                    'menu_type' => 'main-menu'
-                ) ),
-            ) ) );
+    protected function add_menus_to_context( $context ) {
+        $context['menu'] = new TimberMenu( 'primary' );
+        $context['menu_left'] = TimberHelper::function_wrapper( 'wp_nav_menu', array( array(
+            'theme_location' => 'primary',
+            'container' => false,
+            'depth' => 0,
+            'items_wrap' => '<ul class="left">%3$s</ul>',
+            //'fallback_cb' => array( 'TimberFoundation_Walker', 'render_menu_fallback' ),
+            'fallback_cb' => false,
+            'walker' => new TimberFoundation_Walker( array(
+                'in_top_bar' => true,
+                'add_dividers' => true,
+                'item_type' => 'li',
+                'menu_type' => 'main-menu'
+            ) ),
+        ) ) );
 
-            $context['menu_right'] = TimberHelper::function_wrapper( 'wp_nav_menu', array( array(
-                'theme_location' => 'topbar-right',
-                'container' => false,
-                'depth' => 0,
-                'items_wrap' => '<ul class="right">%3$s</ul>',
-                'fallback_cb' => false,
-                'walker' => new TimberFoundation_Walker( array(
-                    'in_top_bar' => true,
-                    'add_dividers' => true,
-                    'item_type' => 'li',
-                    'menu_type' => 'main-menu'
-                ) ),
-            ) ) );
+        $context['menu_right'] = TimberHelper::function_wrapper( 'wp_nav_menu', array( array(
+            'theme_location' => 'topbar-right',
+            'container' => false,
+            'depth' => 0,
+            'items_wrap' => '<ul class="right">%3$s</ul>',
+            'fallback_cb' => false,
+            'walker' => new TimberFoundation_Walker( array(
+                'in_top_bar' => true,
+                'add_dividers' => true,
+                'item_type' => 'li',
+                'menu_type' => 'main-menu'
+            ) ),
+        ) ) );
 
-            // Footer
-            $context['footer_menu'] = new TimberMenu( 'footer' );
+        // Footer
+        $context['footer_menu'] = new TimberMenu( 'footer' );
 
-            return $context;
-        }
+        return $context;
+    }
 
-    function add_to_twig($twig){
+    function add_to_twig( $twig ) {
         // Foundation
         $twig->addFunction( 'pagination', new Twig_SimpleFunction( 'pagination', array( 'TimberFoundationTemplate', 'pagination' ) ) );
 
         // WP API
-        $twig->addFunction(new Twig_SimpleFunction( 'is_active_sidebar', 'is_active_sidebar' ) );
+        $twig->addFunction( new Twig_SimpleFunction( 'is_active_sidebar', 'is_active_sidebar' ) );
 
         return parent::add_to_twig( $twig );
     }
@@ -239,31 +243,30 @@ class TimberFoundationSite extends TimberRegionsSite {
         add_filter( 'wp_title', array( $this, 'wp_title' ), 10, 2 );
     }
 
-        /**
-         * Based on twentyfourteen_wp_title
-         */
-        public function wp_title( $title, $sep ) {
-            global $paged, $page;
+    /**
+     * Based on twentyfourteen_wp_title
+     */
+    public function wp_title( $title, $sep ) {
+        global $paged, $page;
 
-            if ( is_feed() ) {
-                return $title;
-            }
-
-            // Add the site name.
-            $title .= get_bloginfo( 'name', 'display' );
-
-            // Add the site description for the home/front page.
-            $site_description = get_bloginfo( 'description', 'display' );
-            if ( $site_description && ( is_home() || is_front_page() ) ) {
-                $title = "$title $sep $site_description";
-            }
-
-            // Add a page number if necessary.
-            if ( $paged >= 2 || $page >= 2 ) {
-                $title = "$title $sep " . sprintf( __( 'Page %s', 'timber-foundation' ), max( $paged, $page ) );
-            }
-
+        if ( is_feed() ) {
             return $title;
         }
 
+        // Add the site name.
+        $title .= get_bloginfo( 'name', 'display' );
+
+        // Add the site description for the home/front page.
+        $site_description = get_bloginfo( 'description', 'display' );
+        if ( $site_description && ( is_home() || is_front_page() ) ) {
+            $title = "$title $sep $site_description";
+        }
+
+        // Add a page number if necessary.
+        if ( $paged >= 2 || $page >= 2 ) {
+            $title = "$title $sep " . sprintf( __( 'Page %s', 'timber-foundation' ), max( $paged, $page ) );
+        }
+
+        return $title;
+    }
 }
